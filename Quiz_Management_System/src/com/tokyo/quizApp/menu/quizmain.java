a20352e724d9e0d35350a9db566cfa136ae85996
sandesh <sandeshsuryawanshi415@gmail.com>
2025-12-16 09:59:48 +0530

    demo 1

---
+++ Quiz_Management_System/src/com/tokyo/quizApp/menu/quizmain.java
@@ -0,0 +1,15 @@
+package com.tokyo.quizApp.menu;
+
+
+
+import java.util.Scanner;
+public class quizmain {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner (System.in);
+		
+	}
+
+}
